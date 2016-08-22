4c2a654365b296169bf50cfea2a336c8922f0d97
kvnneff <kvnneff@gmail.com>
2016-08-22 16:38:34 -0700

    Place comment css with comment component.

--- src/components/comments/list.js
+++ src/components/comments/list.js
@@ -1,12 +1,26 @@
 const h = require('choo/html')
+const sf = require('sheetify')
 const CommentItem = require('./item')
+
+const prefix = sf`
+  :host > li {
+    padding: 1rem;
+    margin: 2rem 0;
+    background-color: #f4f4f4;
+    border: 1px solid #ccc;
+  }
+
+  :host > li:nth-child(even) {
+    background-color: #fefefe;
+  }
+`
 
 const CommentsList = state => {
   const comments = state.comments
 
   if (!comments || !comments.length) return ''
 
-  return h`<ul class="CommentList list pl0 mt4">
+  return h`<ul class="${prefix} CommentList list pl0 mt4">
     ${comments.map(comment => {
       return CommentItem({ item: comment })
     })}
